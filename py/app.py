ad955ffd6f22108083022a0a6e56b2c16735ec9b
Katherine Door <kittydoordev@gmail.com>
2018-03-22 11:54:38 +0100

    Fixed decode not stripping the content type header

--- py/app.py
+++ py/app.py
@@ -5,9 +5,20 @@ from ml_ocr import img2str
 
 @app.route("/", methods=['POST'])
 def stringify():
+    # get the img data encoded in b64
     base64_img = request.form['img']
-    img = base64.b64decode(base64_img)
-    recognized_str = img2str(base64_img)
+
+    # strip the string 'data:image/png;base64,'
+    b64_stripped = base64_img[22:]
+
+    # decode into binary img
+    img = base64.b64decode(b64_stripped)
+
+    # write to test file
+    #with open('test.png', 'wb') as w:
+    #    w.write(img)
+
+    recognized_str = img2str(img)
     return jsonify(
                 text=recognized_str
             )
