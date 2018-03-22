46345bc8c85e54dc35d15fd0317be444f0a3c29d
awesan1 <jouke@substrate.it>
2018-03-22 18:11:00 +0100

    add path

--- FIODraw/Program.cs
+++ FIODraw/Program.cs
@@ -20,7 +20,7 @@ namespace FIODraw
         public static IWebHost BuildWebHost(string[] args) {
             return WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseUrls($"http://0.0.0.0:5050")
+                .UseUrls($"http://0.0.0.0:5050/draw")
                 .Build();
         }
     }
